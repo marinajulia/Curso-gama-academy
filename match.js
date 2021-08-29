fb0e3ba5e4d2b08913fcad08eec79edbb25beee1
marinajulia <marinajulia707@gmail.com>
2021-08-29 10:38:50 -0300

    api

--- match.js
+++
@@ -1,4 +0,0 @@
-const cpf = "Meu CPF é 461.459.808-00";
-const regex = new RegExp('[0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]{2}');
-
-console.log(cpf.match(regex));
--- template string/templateString.js
+++
@@ -1,6 +0,0 @@
-let nome = 'jc';
-let sobrenome = 'tete';
-let profissao = 'loko';
-
-console.log(`Meu nome é ${nome} ${sobrenome} e minha profssão é ${profissao}`);
-
